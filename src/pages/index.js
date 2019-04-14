258734947a8788512d8b3a5a59d30fa412dde6b2
Dennis Timmers <dennistimmers@burst-digital.com>
2019-04-14 14:56:52 +0200

    add nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,77 @@
 import React from "react"
-import { Link, useStaticQuery } from 'gatsby';
-import Img from 'gatsby-image';
+import { useStaticQuery, Link } from 'gatsby';
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Img from "gatsby-image"
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
+import '../components/CaseSlider/styles/CaseSlider.css';
 
-import ContactList from '../components/ContactList'
-
-import styles from '../components/AboutHeader/styles/About.module.css';
-import '../components/Home/styles/home.css';
-
-const IndexPage = () => {
+const CasesPage = () => {
   const data = useStaticQuery(graphql`
-  query home {
-    portrait: file(relativePath: { eq: "about-me-photo.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 550, quality: 100) {
-          ...GatsbyImageSharpFluid
+    query CasePreview {
+      casePaard: file(relativePath: { eq: "paard-interface.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      caseFood: file(relativePath: { eq: "food.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      woonstadCase: file(relativePath: { eq: "werkstad-logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
         }
       }
     }
-  }
-`)
+  `)
 
   return (
     <Layout>
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <header className={styles.header}>
-        <h1 className={styles.title}>Hello!</h1>
-        <div className={styles.image}>
-          <Img fluid={data.portrait.childImageSharp.fluid} />
+      <header className="cases-header">
+        <div className="nav-link-conainer">
+          <span className="h1 nav-link active" to="/">Cases</span>
+          <Link className="h1 nav-link" to="/">Cases</Link>
         </div>
-        <div className="home-intro">
-          <ContactList />
-          <h2 className="h3">I'm Jiska Heringa I like <span className="highlighted">UX/UI design</span></h2>
-          <p>Alright! so I’m 23-year old based in Rotterdam. In 2016 I graduated in Interactive design (English) at Grafisch Lyceum Rotterdam. The study focused on branding and web coding using HTML/CSS and the basics of javascript and PHP. Currently, I’m studying at Hogeschool Rotterdam (CMD), Where I’m focussing on UX/UI and concept.</p>
-          <p>In my spare time, I work as a chef. Also, I like to practise music, playing the guitar and piano. Or might be found on the beach catching waves (:</p>
+        <div className="nav-link-conainer">
+          <span className="h1 nav-link" to="about">About</span>
+          <Link className="h1 nav-link" to="about">About</Link>
         </div>
       </header>
 
-      <div className="nav-link-conainer">
-        <span className="nav-link" to="cases">Cases</span>
-        <Link className="nav-link" to="cases">Cases</Link>
-      </div>
+      <ParallaxProvider>
+        <div className="cases-wrapper">
+          <Link className="cases" to="food">
+            <div className="cases-visual">
+              <span className="h3 cases-title top">Paard</span>
+              <span className="h3 cases-title bottom">Paard</span>
+              <Parallax y={[-10, 10]} tagOuter="figure">
+                <Img className="cases-image" fluid={data.casePaard.childImageSharp.fluid} />
+              </Parallax>
+              <h3 className="cases-title">Paard</h3>
+            </div>
+          </Link>
+
+          <Link className="cases" to="food">
+            <div className="cases-visual">
+              <span className="h3 cases-title top">Food</span>
+              <span className="h3 cases-title bottom">Food</span>
+              <Parallax y={[-10, 10]} tagOuter="figure">
+                <Img className="cases-image" fluid={data.caseFood.childImageSharp.fluid} />
+              </Parallax>
+              <h3 className="cases-title">Food</h3>
+            </div>
+          </Link>
+        </div>
+      </ParallaxProvider>
     </Layout>
   )
 }
 
-export default IndexPage
+export default CasesPage
